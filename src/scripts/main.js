e70b034103a7e23b2c9a07e59b97204a14e6c9d1
sirserg <sirserg05@gmail.com>
2025-04-04 15:28:54 +0300

    feat: add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.getAttribute('id');
+  label.textContent = input.getAttribute('name');
+
+  input.before(label);
+
+  input.setAttribute('placeholder', input.getAttribute('name'));
+
+});
